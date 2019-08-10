0e51aee32bab78e994099704b964de8afd63ba3d
Sal Garcia <sal.garcia.three@gmail.com>
2019-08-09 18:27:30 -0700

    Added First Files
    Added First Files

---
+++ input_handlers.py
@@ -0,0 +1,23 @@
+import tcod as libtcod
+
+def handle_keys(key):
+    #movement key handling
+    if key.vk == libtcod.KEY_UP:
+        return  {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1, 0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Setting fullscreen with Alt + Enter
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #exit the game
+        return {'exit': True}
+
+    #no key was pressed
+    return {}
